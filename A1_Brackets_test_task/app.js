ba7ca770baeed20b97c22c55d07a20dd3e35a5a7
Ismoil Hasanov <ismail@pop-os.localdomain>
2021-04-11 16:19:39 +0300

    fix: adde some more comments

--- A1_Brackets_test_task/app.js
+++ A1_Brackets_test_task/app.js
@@ -1,13 +1,16 @@
 function parenthesesAreBalanced(string) {
-    let parentheses = "[]{}()", stack = [], i, character, bracePosition;
+    let parentheses = "[]{}()",
+        stack = [], i, character, bracePosition; // creating array and variables 
 
     for (i = 0; character = string[i]; i++) {
         bracePosition = parentheses.indexOf(character);
-
+        //Checking position of braces
         if (bracePosition === -1) {
             continue;
         }
 
+        //Checking position of braces
+        //if positions are correct we are pushing it to array else returning false
         if (bracePosition % 2 === 0) {
             stack.push(bracePosition + 1); // push next expected brace position
         } else {
@@ -23,6 +26,7 @@ function parenthesesAreBalanced(string) {
 
 console.log("Please enter your line of ASCII symbols")
 
+// Simply taking input from terminal and passing it to above written function
 process.stdin.on('data', (chunk) => {
     let inputFromUser = chunk.toString().trim();
     console.log(parenthesesAreBalanced(inputFromUser));
