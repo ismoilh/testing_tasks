30f9a6246a8d2b56312466c909aca8826459ef93
Ismoil Hasanov <ismail@pop-os.localdomain>
2021-04-11 16:05:43 +0300

    feat: balanced parantheses done

---
+++ A1_Brackets_test_task/README.md
@@ -0,0 +1,25 @@
+# Balanced parentheses test NodeJs
+
+## Author: Ismoil Hasanov
+
+### Node - v14.15.4
+
+### npm - 6.14.10
+
+### 1st way to start app, simply use node app.js in root folder
+
+```
+node app.js
+```
+
+### 2nd way to start app, use npm start in root folder
+
+```
+npm start
+```
+
+### 3rd way to start app, use make in root folder
+
+```
+make run
+```
---
+++ A1_Brackets_test_task/app.js
@@ -0,0 +1,29 @@
+function parenthesesAreBalanced(string) {
+    let parentheses = "[]{}()", stack = [], i, character, bracePosition;
+
+    for (i = 0; character = string[i]; i++) {
+        bracePosition = parentheses.indexOf(character);
+
+        if (bracePosition === -1) {
+            continue;
+        }
+
+        if (bracePosition % 2 === 0) {
+            stack.push(bracePosition + 1); // push next expected brace position
+        } else {
+            if (stack.length === 0 || stack.pop() !== bracePosition) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+
+console.log("Please enter your line of ASCII symbols")
+
+process.stdin.on('data', (chunk) => {
+    let inputFromUser = chunk.toString().trim();
+    console.log(parenthesesAreBalanced(inputFromUser));
+});
